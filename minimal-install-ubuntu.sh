facf45ba03dfa122161bad7af877659ffeb8598e
ki <kisemichuk@gmail.com>
2023-11-08 09:58:57 +0400

    edit

--- minimal-install-ubuntu.sh
+++ minimal-install-ubuntu.sh
@@ -1,9 +1,5 @@
 echo "//////////////////////minimal install ubuntu//////////////////////"
-sudo apt install kde-plasma-desktop latte-dock zsh timeshift konsole sddm ark zip unzip
+sudo apt install kde-plasma-desktop latte-dock zsh timeshift konsole sddm ark zip unzip dotnet-sdk-6.0 flameshot
 sudo apt purge kwrite konqueror kwalletmanager plasma-discover plasma-discover-common snapd
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-wget https://github.com/dracula/konsole/archive/master.zip
-unzip  master.zip 
-cd konsole-master
-cp Dracula.colorscheme ~/.local/share/konsole
 echo "//////////////////////installation is finished//////////////////////"
