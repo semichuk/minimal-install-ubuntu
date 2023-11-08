ca2f6f25ee9699bc4a6f9c6f3416740a026cd70c
ki <kisemichuk@gmail.com>
2023-11-08 10:20:13 +0400

    transmission

--- minimal-install-ubuntu.sh
+++ minimal-install-ubuntu.sh
@@ -1,5 +1,5 @@
 echo "//////////////////////minimal install ubuntu//////////////////////"
-sudo apt install kde-plasma-desktop latte-dock zsh timeshift konsole sddm ark zip unzip dotnet-sdk-6.0 flameshot
+sudo apt install kde-plasma-desktop latte-dock zsh timeshift konsole sddm ark zip unzip dotnet-sdk-6.0 flameshot transmission
 sudo apt purge kwrite konqueror kwalletmanager plasma-discover plasma-discover-common snapd
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 echo "//////////////////////installation is finished//////////////////////"
