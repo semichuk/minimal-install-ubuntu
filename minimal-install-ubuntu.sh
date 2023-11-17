aa6ad5de7212aa179fa8c0cffc52871e4ebcbe4a
ki <kisemichuk@gmail.com>
2023-11-17 08:51:42 +0400

    add ffmpeg lame

--- minimal-install-ubuntu.sh
+++ minimal-install-ubuntu.sh
@@ -1,6 +1,6 @@
 echo "//////////////////////minimal install ubuntu//////////////////////"
 #put in /home/ki/.local/share/plasma/look-and-feel/com.github.vinceliuice.WhiteSur-dark/
-sudo apt install kde-plasma-desktop latte-dock zsh timeshift konsole sddm ark zip unzip dotnet-sdk-6.0 flameshot transmission krita neofetch
+sudo apt install kde-plasma-desktop latte-dock zsh timeshift konsole sddm ark zip unzip dotnet-sdk-6.0 flameshot transmission krita neofetch elisa ffmpeg lame
 sudo apt purge kwrite konqueror kwalletmanager plasma-discover plasma-discover-common snapd
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 echo "//////////////////////installation is finished//////////////////////"
