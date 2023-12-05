867f0d6a8ef6d0b1af8fcf6eb11d9bf320f72384
kisemichuk <70934055+semichuk@users.noreply.github.com>
2023-12-05 20:46:05 +0000

    Update minimal-install-ubuntu.sh

--- minimal-install-ubuntu.sh
+++ minimal-install-ubuntu.sh
@@ -1,6 +1,7 @@
 echo "//////////////////////minimal install ubuntu//////////////////////"
 #put in /home/ki/.local/share/plasma/look-and-feel/com.github.vinceliuice.WhiteSur-dark/
-sudo apt install kde-plasma-desktop latte-dock zsh timeshift konsole sddm ark zip unzip dotnet-sdk-6.0 flameshot transmission krita neofetch elisa ffmpeg lame
+sudo apt install kde-plasma-desktop latte-dock zsh timeshift konsole sddm ark zip unzip dotnet-sdk-6.0 flameshot transmission krita neofetch elisa ffmpeg lame flatpak
 sudo apt purge kwrite konqueror kwalletmanager plasma-discover plasma-discover-common snapd
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo    
 echo "//////////////////////installation is finished//////////////////////"
