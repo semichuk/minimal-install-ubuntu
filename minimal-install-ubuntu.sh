719ee702a95232cc8b9c19143a5fcea9f1434703
kisemichuk <70934055+semichuk@users.noreply.github.com>
2024-08-06 18:18:56 +0400

    Update minimal-install-ubuntu.sh

--- minimal-install-ubuntu.sh
+++ minimal-install-ubuntu.sh
@@ -1,8 +1,7 @@
 echo "//////////////////////minimal install ubuntu//////////////////////"
 #put in /home/ki/.local/share/plasma/look-and-feel/com.github.vinceliuice.WhiteSur-dark/
-sudo apt install kde-plasma-desktop latte-dock zsh timeshift konsole sddm ark zip unzip dotnet-sdk-6.0 flameshot transmission krita neofetch elisa ffmpeg lame flatpak network-manager pulseaudio-module-bluetooth plasma-nm
+sudo apt install kde-plasma-desktop latte-dock zsh timeshift konsole sddm ark zip unzip dotnet-sdk-8.0 flameshot transmission krita neofetch elisa ffmpeg lame flatpak network-manager pulseaudio-module-bluetooth plasma-nm dolphin
 sudo apt purge kwrite konqueror kwalletmanager plasma-discover plasma-discover-common snapd
-sudo apt install dolphin
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo    
 echo "//////////////////////installation is finished//////////////////////"
