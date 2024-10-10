6a6cb7444cce784116c9d02fea1fa5e2a5bd4355
ki <kisemichuk@gmail.com>
2024-10-10 17:10:39 +0400

    edit

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo "//////////////////////minimal install ubuntu//////////////////////"
-sudo apt install kde-plasma-desktop latte-dock zsh timeshift konsole sddm ark zip unzip dotnet-sdk-8.0 flameshot transmission krita neofetch elisa ffmpeg lame flatpak network-manager pulseaudio-module-bluetooth plasma-nm dolphin wireguard
+sudo apt install kde-plasma-desktop latte-dock zsh timeshift konsole sddm ark zip unzip dotnet-sdk-8.0 flameshot transmission krita neofetch elisa ffmpeg lame flatpak network-manager pulseaudio-module-bluetooth plasma-nm dolphin wireguard inkscape libreoffice
 #sudo apt purge kwrite konqueror kwalletmanager plasma-discover plasma-discover-common snapd
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
