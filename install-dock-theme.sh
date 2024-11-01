9f9ceb7d31438083e1f8e118be89eda24cd3c1cd
ki <kisemichuk@gmail.com>
2024-11-02 00:59:06 +0400

    add latte layout and plasmoid

--- install-dock-theme.sh
+++
@@ -1,1 +0,0 @@
-sudo cp ./dock/'My Layout.layout.latte' /home/$USER/.config/latte/
--- install.sh
+++ install.sh
@@ -2,19 +2,21 @@ echo "//////////////////////minimal install ubuntu//////////////////////"
 sudo apt install kde-plasma-desktop latte-dock zsh timeshift konsole sddm ark zip unzip dotnet-sdk-8.0 flameshot transmission krita neofetch elisa ffmpeg lame flatpak network-manager pulseaudio-module-bluetooth plasma-nm dolphin wireguard inkscape libreoffice -y
 
 #sudo apt purge kwrite konqueror kwalletmanager plasma-discover plasma-discover-common snapd
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+sudo apt purge snapd -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 git clone https://github.com/semichuk/linux-icon-theme.git
 git clone https://github.com/semichuk/linux-cursors.git
 git clone https://github.com/semichuk/kde-rounded-corners.git
-git clone https://github.com/semichuk/plasma-active-application.git
-git clone https://github.com/semichuk/linux-kde-theme.git
+git clone https://github.com/semichuk/WhiteSur-kde.git
 git clone https://github.com/semichuk/minimal-install-terminal.git
+
+mkdir /home/$USER/.config/latte/
+sudo cp ki.layout.latte /home/$USER/.config/latte/Default.layout.latte
 
 sudo bash linux-icon-theme/install.sh -b
 sudo bash linux-cursor/install.sh
-plasmapkg2 -i plasma-active-application
-sudo bash linux-kde-theme/install.sh -w sharp
+plasmapkg2 -i plasma-active-application.plasmoid
+sudo bash WhiteSur-kde/install.sh -w sharp
 sudo apt install git cmake g++ extra-cmake-modules kwin-dev libkf5configwidgets-dev -y
 cd kde-rounded-corners
 mkdir build
@@ -24,5 +26,6 @@ cmake --build . -j
 sudo make install
 cd ..
 cd ..
-sudo bash minimal-install-terminal/install.sh
+cd minimal-install-terminal/
+sudo bash install.sh
 echo "//////////////////////installation is finished//////////////////////"
